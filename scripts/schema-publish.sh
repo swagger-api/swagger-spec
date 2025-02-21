5ba8fba10cb93f3dd6e76c4f9794b41f5d28b64c
Duncan Beevers <duncan@dweebd.com>
2025-02-20 16:00:52 -0800

    fixup! Update schema-publish.sh

--- scripts/schema-publish.sh
+++ scripts/schema-publish.sh
@@ -29,7 +29,36 @@ publish_schema() {
   node scripts/yaml2json/yaml2json.js "$target.yaml"
   rm "$target.yaml"
   mv "$target.json" "$target"
-  echo " * $newestCommitDate: $schema"
+
+  # Find the jekyll lander markdown file for this iteration.
+  
+  # Define jekyllLander by referencing a markdown file whose name matches the
+  # iteration date. This approach would work with today's jekyll landers
+  # local jekyllLander=$deploydir/$base/$date.md
+  
+  # Define jekyllLander by referencing a static file
+  # local jekyllLander=$deploydir/$base/latest.md
+  
+  # Define jekyllLander by finding the first markdown file already-present in the
+  # deploy directory.
+  # local jekyllLander=$(find "$deploydir/$base" -maxdepth 1 -name "*.md" | head -n 1)
+
+  # Define jekyllLander by finding all the markdown files already-present in the
+  # deploy directory. If multiple files are present, this script will fail as it
+  # attempts to move these multiple files to a single $target.md
+  # This behavior is the closest match to that of the previous version of the
+  # schema-publish script.
+  local jekyllLander=$(find "$deploydir/$base" -maxdepth 1 -name "*.md")
+
+  # Move the jekyll lander markdown for this iteration to the deploy destination.
+  # The lander files only exist in the gh-pages branch.
+  if [ ! -z "$jekyllLander" ]; then
+    mv $jekyllLander $target.md
+    echo " * $newestCommitDate: $schema & jekyll lander $(basename $jekyllLander)"
+  else
+    echo " * $newestCommitDate: $schema"
+  fi
+
 }
 
 echo === Building schemas into $deploydir
